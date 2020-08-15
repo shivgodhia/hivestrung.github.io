32461701037126c67f8827d25fcae40b2e9cb069
Shiv Godhia <shivgodhia@gmail.com>
2020-08-15 13:16:48 +0100

    removed pensieve posts, brittany stuff

---
+++ content/posts/template/index.md
@@ -0,0 +1,11 @@
+---
+title: Template
+description: Template for blog posts
+date: '2019-12-13'
+draft: true
+slug: '/pensieve/template'
+tags:
+  - template
+---
+
+## Template
--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,8 +123,8 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/pensieve" />
+        <title>Pensieve | Shiv Godhia</title>
+        <link rel="canonical" href="https://shivgodhia.tech/pensieve" />
       </Helmet>
 
       <StyledMainContainer>
