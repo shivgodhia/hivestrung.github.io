697450d184bf188e6963408434e04bfb9b3405db
Shiv Godhia <shivgodhia@gmail.com>
2020-08-15 11:17:47 +0100

    greyscale 30% for profile pic, not tinted

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -63,7 +63,7 @@ const StyledPic = styled.div`
 const StyledAvatar = styled(Img)`
   position: relative;
   mix-blend-mode: multiply;
-  filter: grayscale(100%) contrast(1);
+  filter: grayscale(30%) contrast(1);
   border-radius: ${theme.borderRadius};
   transition: ${theme.transition};
 `;
@@ -72,7 +72,7 @@ const StyledAvatarLink = styled.a`
   width: 100%;
   position: relative;
   border-radius: ${theme.borderRadius};
-  background-color: ${colors.green};
+  background-color: ${colors.white};
   margin-left: -20px;
   &:hover,
   &:focus {
