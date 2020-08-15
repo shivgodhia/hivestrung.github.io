a83b087ca69c9febe59158b9c1df09ca5408c61b
Shiv Godhia <shivgodhia@gmail.com>
2020-08-15 13:53:13 +0100

    posts should say they're from pensieve, not my name

--- src/templates/post.js
+++ src/templates/post.js
@@ -42,7 +42,7 @@ const PostTemplate = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>{title} | Shiv Godhia</title>
+        <title>{title} | Pensieve</title>
         <link rel="canonical" href="https://shivgodhia.tech/pensieve" />
       </Helmet>
 
